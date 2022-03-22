03ff4a7ae000c4e62e55511d883a0c7f7984d41f
David Fuller <david@muvi2.co.uk>
2022-03-22 10:08:58 +0000

    Initial commit

---
+++ settings.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const fs = require('fs').promises
+
+
+async function savePages(pages, userPath){
+  console.log("Settings")
+  console.log(userPath)
+  let filename = path.join(userPath,'pages.json');
+  console.log(filename)
+  console.log(JSON.stringify(pages, null, 4));
+  await fs.writeFile(filename,JSON.stringify(pages, null, 4));
+  
+}
+
+async function readPages(userPath){
+  let filename = path.join(userPath,'pages.json');
+  let data = await fs.readFile(filename);
+  return JSON.parse(data);
+}
+
+module.exports = {savePages, readPages}
