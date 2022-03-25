a1f0743d8fd73eba3d41ed9f4276d52a2b8fcc63
David Fuller <david@muvi2.co.uk>
2022-03-25 10:00:28 +0000

    Lots of new stuff wrt to jpeg Export

--- xmlStrings.js
+++ xmlStrings.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const root = {
     open:'<PixelXML xmlns="http://pixelpower.com/PixelXML">', 
     close: '</PixelXML>'
@@ -216,6 +217,130 @@ function playMedia(clientId, level, speed, playerId){
   
   return temp
 }
+/**
+ * 
+ * @param {string} ppwgFilename 
+ * @param {object} pageDetails 
+ * @param {string} jobFolder
+ * @returns {object}
+*/
+function pageDetailsFromPPWG(ppwgXML, pageDetails){
 
+  var tempDetails = extractJobAndPage(ppwgXML);
+  //let tempPageDetails = {...pageDetails};
 
-module.exports = {root, setLayerState, doTrigger, layerStateChange, layer, state, source, systemTrigger, ppwgToAsset, createPageXML, createPpwlXML, attachMedia, media, playMedia}
+  if (tempDetails === null){
+  } else {
+    pageDetails.jobName = path.join(pageDetails.jobPath, tempDetails.jobName);
+    pageDetails.page = tempDetails.page;
+    var fieldData = extractFieldData(ppwgXML)
+    if (fieldData === null){
+    } else {
+      pageDetails.fieldData = fieldData;
+    }
+  }
+  return pageDetails
+}
+/**
+ * 
+ * @param {string} xmlString 
+ * @returns {object}
+ */
+function extractJobAndPage(xmlString){
+
+  const startBit = '<Video FileName="'
+  const endBit = '">';
+
+  var start = xmlString.indexOf(startBit) + startBit.length;
+  var end = xmlString.indexOf(endBit, start);
+
+  if (start > -1 && end > -1){
+    var combined = xmlString.substring(start, end);
+    var split = combined.split('\\');
+    if (split.length == 2){
+      var result = {}
+      result.jobName = split[0]
+      result.page = split[1]
+      return result
+    } else {
+      return null
+    }
+  } else {
+    return null;
+  }
+}
+/**
+ * 
+ * @param {string} xmlString 
+ * @returns {string}
+*/
+function extractFieldData(xmlString){
+  const startBit = '<OverrideData>';
+  const endBit = '</OverrideData>';
+
+  var start = xmlString.indexOf(startBit);
+  var end = xmlString.indexOf(endBit, start) + endBit.length;
+
+  if (start > -1 && end > -1){
+    return xmlString.substring(start, end);
+  } else {
+    return null;
+  }
+}
+
+function imageXMLPPWG(pageDetails, width, height){
+  /**
+   * @type {string}
+   */
+  var tempXML;
+
+  tempXML = root.open;
+  tempXML += mediaInfo.open;
+  tempXML += request.open;
+  tempXML += mediaNameWithOverrideData(pageDetails);
+  tempXML += preview(width, height);
+  tempXML += request.close;
+  tempXML += mediaInfo.close;
+  tempXML += root.close;
+
+  return tempXML;
+
+  //var getImageDirect ='<PixelXML xmlns="http://pixelpower.com/PixelXML"><GetMediaInfo Device="Clip" Category="Manage"><DefaultTimecodeRate FrameRate="TestHarness.Default" DropFrame="false" />
+  //<Request><Media Name="\\\\pomatter\\Assets\\Create\\TV3S_CREATE_MASTER.cgz\\1210"></Media><Preview AutoCrop="false" Category="PreviewFrames"><Size Width="512" Height="288" /></Preview></Request></GetMediaInfo></PixelXML>'
+}
+const mediaInfo = {
+  open:   '<GetMediaInfo Device="Clip" Category="Manage"><DefaultTimecodeRate FrameRate="TestHarness.Default" DropFrame="false" />',
+  close:  '</GetMediaInfo>'
+}
+
+const request = {
+  open:   '<Request>',
+  close:  '</Request>'
+}
+
+function mediaNameWithOverrideData(pageDetails){
+  var tempXML;
+
+  tempXML = '<Media Name="' + pageDetails.jobName +'\\' + pageDetails.page + '">';
+  tempXML += pageDetails.fieldData;
+  tempXML += '</Media>';
+  return tempXML
+}
+
+/**
+ * 
+ * @param {number} width 
+ * @param {number} height 
+ * @returns {string}
+ */
+ function preview(width, height){
+
+  return '<Preview AutoCrop="false" Category="PreviewFrames"><Size Width="' + width + '" Height="' + height + '" /></Preview>'
+}
+
+const imageDataTag = {
+  open: '<ImageData>',
+  close:  '</ImageData>'
+}
+
+module.exports = {root, setLayerState, doTrigger, layerStateChange, layer, state, source, systemTrigger, ppwgToAsset, createPageXML, createPpwlXML, attachMedia, media, playMedia, pageDetailsFromPPWG, imageXMLPPWG, imageDataTag}
