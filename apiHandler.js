6fbe3a4d9018989912c26ffa0c280e86460199eb
David Fuller <david@muvi2.co.uk>
2022-09-22 09:46:25 +0100

    http get

---
+++ apiHandler.js
@@ -0,0 +1,32 @@
+function httpGet(url) {
+  return new Promise((resolve, reject) => {
+    const http = require('http'),
+      https = require('https');
+
+    let client = http;
+
+    if (url.toString().indexOf("https") === 0) {
+      client = https;
+    }
+
+    client.get(url, (resp) => {
+      let chunks = [];
+
+      // A chunk of data has been recieved.
+      resp.on('data', (chunk) => {
+        chunks.push(chunk);
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        resolve(Buffer.concat(chunks));
+      });
+
+    }).on("error", (err) => {
+      reject(err);
+    });
+  });
+}
+
+
+module.exports = {httpGet}
